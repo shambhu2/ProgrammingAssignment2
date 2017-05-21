f8c510dd1e7c3229a138ada5c83262258424dbcc
shambhu2 <shambhu_n@yahoo.com>
2017-05-22 01:16:16 +0530

    W3 assignment- cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,27 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  inv_v <<- cacheSolve(x)
+  
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  m2 <<- inv_v
+  
+  if(!is.null(m2))
+  {
+    print("getting cached data")
+    print(m2)
+    
+  }
+  else 
+    m <<- solve(x)
+  
 }
+
